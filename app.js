1e0b7a9dd90fad43ce0eb1206dbbbd60e43a81b9
Misha <wrongsky1@yandex.ru>
2024-04-21 20:17:59 +0300

    add cors

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mainRoute = require("./routes/main");
 const gamesRouter = require("./routes/games");
+const cors = require("./middlewares/cors");
 
 const PORT = 3000;
 const app = express();
 
-app.use(bodyParser.json(), express.static(path.join(__dirname, "public")), mainRoute, gamesRouter);
+app.use(
+  cors,
+  bodyParser.json(), express.static(path.join(__dirname, "public")), mainRoute, gamesRouter);
 
 app.listen(PORT, () => {
   console.log(`Приложение запущено тут: http://localhost:${PORT}`);
