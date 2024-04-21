a86a19283d865b77d2358547f4f6440fc1eb6edb
Misha <wrongsky1@yandex.ru>
2024-04-21 20:06:32 +0300

    добавил бодипарсер

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require("express");
 
 const path = require("path");
-
+const bodyParser = require("body-parser");
 const mainRoute = require("./routes/main");
 const gamesRouter = require("./routes/games");
 
 const PORT = 3000;
 const app = express();
 
-app.use(
-  express.static(path.join(__dirname, "public")),
-  mainRoute,
-  gamesRouter
-);
+app.use(bodyParser.json(), express.static(path.join(__dirname, "public")), mainRoute, gamesRouter);
 
 app.listen(PORT, () => {
   console.log(`Приложение запущено тут: http://localhost:${PORT}`);
