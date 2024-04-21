edaae9ff79883de58b8a628ff0a94a6551c59a61
Misha <wrongsky1@yandex.ru>
2024-04-21 19:59:40 +0300

    добавил роуты и медлвары

--- routes/games.js
+++ routes/games.js
@@ -1,11 +1,31 @@
+const { sendAllGames, sendUpdatedGames } = require("../controllers/games");
+const {
+  findGameById,
+  updateGamesFile,
+  checkIsTitleInArray,
+  updateGamesArray,
+  getAllGames,
+  deleteGame,
+} = require("../middlewares/games");
+
 const gamesRouter = require("express").Router();
 
-const getAllGames = async (req, res) => {};
-const deleteGame = async (req, res) => {};
-const addGame = async (req, res) => {};
-
-gamesRouter.get("/games", getAllGames);
-gamesRouter.delete("/games/:id", deleteGame);
-gamesRouter.post("/games", addGame);
+gamesRouter.get("/games", getAllGames, sendAllGames);
+gamesRouter.delete(
+  "/games/:id",
+  getAllGames,
+  findGameById,
+  deleteGame,
+  updateGamesFile,
+  sendUpdatedGames
+);
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
 
 module.exports = gamesRouter;
